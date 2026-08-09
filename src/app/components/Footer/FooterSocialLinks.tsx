b5539d13f09ed56160568414da0e658c3f4ef69f
WuKe <118019715+source-code-examples@users.noreply.github.com>
2026-08-09 23:00:50 +0200

    feat(footer): update and add net social media icons and links to footer

--- src/app/components/Footer/FooterSocialLinks.tsx
+++ src/app/components/Footer/FooterSocialLinks.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 import Image from 'next/image'
-import { BsGlobe2, BsTiktok } from 'react-icons/bs'
-import { FaGithub, FaInstagram } from 'react-icons/fa'
+import { BsGlobe2 } from 'react-icons/bs'
+import { FaGithub, FaGithubSquare, FaInstagram } from 'react-icons/fa'
 
 const FooterSocialLinks = () => {
   return (
@@ -9,62 +9,94 @@ const FooterSocialLinks = () => {
       <Link
         href="https://github.com/wkleus/"
         target="_blank"
-        className="text-2xl text-gray-600 opacity-70 transition duration-300 hover:opacity-100 dark:text-gray-300"
+        className="group relative text-2xl text-gray-600 opacity-70 transition duration-300 hover:opacity-100 dark:text-gray-300"
       >
-        <FaGithub />
+        <FaGithub size={26} />
+        <span className="absolute bottom-full left-1/2 mb-2 -translate-x-1/2 rounded-lg bg-gray-900 px-2 py-1 text-[11px] font-medium whitespace-nowrap text-white opacity-0 shadow-lg transition-opacity group-hover:opacity-100 dark:bg-yellow-100 dark:text-black">
+          My GitHub Profile
+          <span className="absolute top-full left-1/2 -translate-x-1/2 border-7 border-transparent border-t-gray-900 dark:border-t-yellow-100"></span>
+        </span>
+      </Link>
+
+      <Link
+        href="https://github.com/Monocle-Labs"
+        target="_blank"
+        className="group relative text-2xl text-gray-600 opacity-70 transition duration-300 hover:opacity-100 dark:text-gray-300"
+      >
+        <FaGithubSquare size={26} />
+        <span className="absolute bottom-full left-1/2 mb-2 -translate-x-1/2 rounded-lg bg-gray-900 px-2 py-1 text-[11px] font-medium whitespace-nowrap text-white opacity-0 shadow-lg transition-opacity group-hover:opacity-100 dark:bg-yellow-100 dark:text-black">
+          Monocle Labs (GitHub)
+          <span className="absolute top-full left-1/2 -translate-x-1/2 border-7 border-transparent border-t-gray-900 dark:border-t-yellow-100"></span>
+        </span>
       </Link>
 
       <Link
         href="https://pixelstack-me.vercel.app/"
         target="_blank"
-        className="text-2xl text-gray-600 opacity-70 transition duration-300 hover:opacity-100 dark:text-gray-300"
+        className="group relative text-2xl text-gray-600 opacity-70 transition duration-300 hover:opacity-100 dark:text-gray-300"
       >
-        <BsGlobe2 />
+        <BsGlobe2 size={25} />
+        <span className="absolute bottom-full left-1/2 mb-2 -translate-x-1/2 rounded-lg bg-gray-900 px-2 py-1 text-[11px] font-medium whitespace-nowrap text-white opacity-0 shadow-lg transition-opacity group-hover:opacity-100 dark:bg-yellow-100 dark:text-black">
+          Demo Vercel Website
+          <span className="absolute top-full left-1/2 -translate-x-1/2 border-7 border-transparent border-t-gray-900 dark:border-t-yellow-100"></span>
+        </span>
       </Link>
 
       <Link
         href="https://www.instagram.com/"
         target="_blank"
-        className="text-2xl text-gray-600 opacity-70 transition duration-300 hover:opacity-100 dark:text-gray-300"
+        className="group relative text-2xl text-gray-600 opacity-70 transition duration-300 hover:opacity-100 dark:text-gray-300"
       >
-        <FaInstagram />
+        <FaInstagram size={26} />
+        <span className="absolute bottom-full left-1/2 mb-2 -translate-x-1/2 rounded-lg bg-gray-900 px-2 py-1 text-[11px] font-medium whitespace-nowrap text-white opacity-0 shadow-lg transition-opacity group-hover:opacity-100 dark:bg-yellow-100 dark:text-black">
+          Instagram
+          <span className="absolute top-full left-1/2 -translate-x-1/2 border-7 border-transparent border-t-gray-900 dark:border-t-yellow-100"></span>
+        </span>
       </Link>
 
       <Link
         href="https://www.wearedevelopers.com/"
         target="_blank"
-        className="text-2xl text-gray-600 transition duration-300 dark:text-gray-300"
+        className="group relative text-2xl text-gray-600 transition duration-300 dark:text-gray-300"
       >
         <Image
           src="/icons/WeAreDevelopers-3.svg"
           alt="WeAreDevelopers"
-          width={48}
-          height={48}
-          className="relative h-7 w-auto opacity-70 invert-70 transition hover:opacity-100 dark:invert-0"
+          width={54}
+          height={54}
+          className="relative h-8 w-auto opacity-70 invert-70 transition hover:opacity-100 dark:invert-0"
         />
+        <span className="absolute bottom-full left-1/2 mb-2 -translate-x-1/2 rounded-lg bg-gray-900 px-2 py-1 text-[11px] font-medium whitespace-nowrap text-white opacity-0 shadow-lg transition-opacity group-hover:opacity-100 dark:bg-yellow-100 dark:text-black">
+          WeAreDevelopers
+          <span className="absolute top-full left-1/2 -translate-x-1/2 border-7 border-transparent border-t-gray-900 dark:border-t-yellow-100"></span>
+        </span>
       </Link>
 
       <Link
         href="https://www.get-in-it.de/"
         target="_blank"
-        className="text-gray-600 transition duration-300 dark:text-gray-300"
+        className="group relative text-gray-600 transition duration-300 dark:text-gray-300"
       >
         <Image
           src="/icons/get-in-it-2.png"
           alt="get-in-it"
-          width={32}
-          height={32}
+          width={34}
+          height={34}
           className="relative -top-1.5 opacity-70 brightness-0 grayscale invert-30 transition hover:opacity-100 dark:invert-70 hover:dark:invert-100"
         />
+        <span className="absolute bottom-full left-1/2 mb-2 -translate-x-1/2 rounded-lg bg-gray-900 px-2 py-1 text-[11px] font-medium whitespace-nowrap text-white opacity-0 shadow-lg transition-opacity group-hover:opacity-100 dark:bg-yellow-100 dark:text-black">
+          get-in-it
+          <span className="absolute top-full left-1/2 -translate-x-1/2 border-7 border-transparent border-t-gray-900 dark:border-t-yellow-100"></span>
+        </span>
       </Link>
 
-      <Link
+      {/* <Link
         href="https://www.tiktok.com/"
         target="_blank"
         className="text-2xl text-gray-600 opacity-70 transition duration-300 hover:opacity-100 dark:text-gray-300"
       >
         <BsTiktok />
-      </Link>
+      </Link> */}
     </div>
   )
 }
